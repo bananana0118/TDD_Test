cad629c66caf36de94fb44a80ce55a2704cb6692
yeajeong kang <bananana0118@gmail.com>
2023-05-28 16:05:46 +0900

    체크박스 업데이트에 따른 버튼 비활성화

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import './App.css';
 
 function App() {
   const [buttonColor, setButtonColor] = useState("red")
+  const [checked, setChecked] = useState(false)
   const newButtonColor = buttonColor === "red" ? "blue" : "red";
   return (
     <div>
       <button style={{ backgroundColor: buttonColor }}
         onClick={() => setButtonColor(newButtonColor)}
+        disabled={checked}
       >Change to {newButtonColor}</button>
+
+      <input type="checkbox" onChange={(e) => { setChecked(e.target.checked) }} defaultChecked={checked} />
     </div >
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import { logRoles } from '@testing-library/react';
 import App from './App';
 
-test('button color initial', () => {
+test('button color initial, and update color and text', () => {
   const { container } = render(<App></App>)
   //find an element with a role of button and text "Change to blue"
   logRoles(container)
@@ -19,4 +19,36 @@ test('button color initial', () => {
 });
 
 
+test("initial conditions", () => {
+  render(<App />)
 
+  //initial
+  const colorButton = screen.getByRole("button", { name: "Change to blue" })
+  expect(colorButton).toBeEnabled()
+
+  //initial
+  const checkBox = screen.getByRole("checkbox")
+  expect(checkBox).not.toBeChecked();
+
+})
+
+test("선택후 비활성화 및 재선택 후 다시활성화", () => {
+  render(<App />)
+  //initial
+  const colorButton = screen.getByRole("button", { name: "Change to blue" })
+  //initial
+  const checkBox = screen.getByRole("checkbox")
+
+  //버튼이 체크되면
+  fireEvent.click(checkBox)
+
+  //버튼이 비활성화 되도록
+  expect(colorButton).toBeDisabled();
+
+  //버튼이 체크되면
+  fireEvent.click(checkBox)
+
+  //버튼이 활성화 되도록
+  expect(colorButton).toBeEnabled();
+
+})
