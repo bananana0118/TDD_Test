0da6b19418cccb008b3644e281a77ffbf8af8901
yeajeong kang <bananana0118@gmail.com>
2023-05-28 15:32:12 +0900

    Test and conde button change

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { logRoles } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText("learn React");
-  expect(linkElement).toBeInTheDocument();
+test('button color initial', () => {
+  const { container } = render(<App></App>)
+  //find an element with a role of button and text "Change to blue"
+  logRoles(container)
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  expect(colorButton).toHaveStyle({ "background-color": "red" })
+
+  //click button
+  fireEvent.click(colorButton);
+  //expect the background color to be blue
+  expect(colorButton).toHaveStyle({ "background-color": "blue" })
+
+  expect(colorButton).toHaveTextContent("Change to red")
+
 });
+
+
+
